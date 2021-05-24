23d57f085a2a941a45d5cbb0bdf36d3d3ed1ad49
ekohli <ekohli@student.cs.uwaterloo.ca>
2021-05-24 09:14:57 -0400

    added files

---
+++ MSDRadixInsertionSort.cpp
@@ -0,0 +1,213 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <chrono>
+#include <assert.h>
+
+
+
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using ns = chrono::nanoseconds;
+using get_time = chrono::steady_clock;
+
+
+using namespace std;
+
+
+//---------------------------------------------------------//
+// BucketSort helper function that is going to be used     // 
+// in MSDSort below                                        //
+//---------------------------------------------------------//
+void bucketSort(vector<unsigned long int> &A, int d, int m) {
+	int n = A.size(); //size of array
+        int val = 1;
+
+	//finds the digit we want to find
+	int digitVal = m - d - 1;
+	while(digitVal != 0) {
+		val = val * 10;
+		digitVal--;
+	}
+
+	//initializes array of empty lists
+	vector<vector<unsigned long int>> B(10);
+
+	//for loop for adding the appropriate values to B
+	for (int t = 0; t < n; t++) {
+		unsigned long int num = (A[t]/val) % 10;
+		B[num].push_back(A[t]);
+	}
+
+	//adding the appropriate values back into A
+	int i = 0;
+	for (int j = 0; j < B.size(); j++) {
+		for (int k = 0; k < B[j].size(); k++) {
+			A[i++] = B[j][k];
+		}
+	}
+}
+
+//---------------------------------------------------------//
+// InsertionSort helper function that is going to be used  // 
+// in MSDSort below                                        //
+//---------------------------------------------------------//
+void insertionSort(vector<unsigned long int> &A, int n) {
+    unsigned long int key = 0;
+    int j = 0;
+    //goes through the values for sorting
+    for (int i = 1; i < n; i++) {
+        key = A[i];
+        j = i - 1;
+        while (j >= 0 && A[j] > key) {
+            A[j + 1] = A[j];
+            j = j - 1;
+        }
+        A[j + 1] = key;
+    }
+} 
+
+//---------------------------------------------------------//
+// MSDSort implemented as in the assignment description    //
+//---------------------------------------------------------//
+
+void MSDSort(vector<unsigned long int> &A, int m, int l, int r, int d, int threshold, int &total_num_calls) {
+	total_num_calls++;
+	int size = A.size();
+	//calls insertion sort of the size is less than or equal to threshold
+	if ((r - l + 1) <= threshold) {
+		insertionSort(A, size);
+	}
+	//if l < r
+	else if (l < r) {
+		total_num_calls++;	
+		//creates a new vector B to hold the values in between l and r
+		vector<unsigned long int> B;
+		for (int i = l; i <= r; i++) {
+			B.push_back(A[i]);
+		}
+		//calls bucket sort on B
+		bucketSort(B, d, m);
+
+		//reassign whatever is in B to A
+		for (int k = 0; k < B.size(); k++) {
+			A[k] = B[k];
+		}
+
+		//checks if there are still digits
+		if (d < m) {
+			int newL = l;
+			while (newL <= r) {
+				//need to split into groups
+				int newR = 0;
+
+				//m-d number of times we need to divide A[i] by 10
+				//creates new temp vector C that holds the digits we need
+				vector<unsigned long int> C;
+				
+				//extracts the appropriate digit from the values in the array
+				for (int t = 0; t < A.size(); t++) {
+					int digitVal = m - d - 1;
+					unsigned long int temp;
+					while (digitVal != 0) {
+						temp = A[t]/10;
+						digitVal--;
+					}
+					int dig = temp % 10;
+					C.push_back(dig);
+				}
+				newR = newL;	
+				//compares side by side values and increases r' by 1 if they are the same
+				for (int s = 0; s < C.size() - 1; s++) {
+					if (C[s] == C[s+1]) {
+						newR++;
+					}
+					else {
+						break;
+					}
+				}
+				//increases total number of calls
+				//recursive call
+				MSDSort(A, m, newL, newR, d+1, threshold, total_num_calls);
+				newL = newR + 1;	
+			}
+		}
+	}
+}
+
+
+//--------------------------------------------//
+// helper function to generage a random array //
+//--------------------------------------------//
+
+void getRandomArray(vector<unsigned long int>& A)
+{
+    for (unsigned int i = 0; i < A.size(); i++)
+        A[i] = (unsigned long int) rand() * (unsigned long int)  rand() * (unsigned long int)  rand();
+
+}
+
+#ifndef TESTING
+int main()
+{
+    int m = 10;
+    int threshold = 2;
+    int n = 100000;
+    vector<unsigned long int> A;
+
+    // for tests on a random array, set this variable to false
+    bool read_input_from_cin = true;
+    
+    if (read_input_from_cin)
+    {
+        cin >> n;  // read the size of array A
+        cin >> threshold;  // read the threshold for the sorting algorithm 
+
+        A.resize(n);
+
+        // read array A from standard input
+        for (int i = 0; i < n; i++) {
+            cin >> A[i];
+	}
+
+    }
+    else {
+        A.resize(n);
+        getRandomArray(A);
+
+    }
+    auto start = get_time::now(); //use auto keyword to minimize typing strokes :)
+
+    int total_num_calls = 0;
+    MSDSort(A, m, 0, n-1, 0, threshold, total_num_calls);
+ 
+    auto end = get_time::now();
+    auto diff = end - start;
+
+    
+    bool sorted = true;
+    // print re-arranged A  to standard output    
+    cout << A[0] << endl;
+    for (int i = 1; i < n; i++)
+    {
+        cout << A[i] << endl;
+        if (A[i] < A[i - 1])
+            sorted = false;       
+    }
+    
+     cout << total_num_calls << endl;
+        
+    // uncomment this line to have running time printed to the standard output
+    //cout << "Time to sort: " <<  chrono::duration <double, milli>(diff).count() << " ms" << endl;
+
+    return 0;
+
+}
+
+#endif
+
---
+++ kWayMergeSort.cpp
@@ -0,0 +1,183 @@
+#include <utility>
+#include <vector>
+#include <stdexcept>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <math.h>
+using namespace std;
+
+
+//-----------------------------------------//
+// a helper function to print arrays       //
+//-----------------------------------------//
+void print(std::vector <int> const &a)
+{
+    for(int i=0; i < a.size(); i++)
+        std::cout << a.at(i) << " ";
+    cout << endl;
+}
+
+//-----------------------------------------//
+// ArgMin(M) is a helper function that     //
+// return the index of the lowest value    //
+//-----------------------------------------//
+int ArgMin(const vector<pair<int,int>> & M) {
+        int l = 0;
+        //gets the size of M
+        int m = M.size();
+
+        //iterates from 0 to m
+        for (int i = 0; i < m; i++) {
+                //comparison between the ith position and the lth position
+                if (M[i].first < M[l].first) {
+                        l = i;
+                }
+        }
+
+        //returns the index l
+        return l;
+}
+
+//-----------------------------------------//
+// splits A into k sub-arrays              //
+//-----------------------------------------//
+vector<vector<int>> split(const vector<int> & A, int k)
+{
+        //size of array
+        int n = A.size();
+        int q = n/k;
+        int r = n%k;
+
+        //sets index to 0
+        int index = 0;
+        int val = 0;
+
+        //creates a temp vectors of vectors to return
+        vector<vector<int>> newA;
+
+        for (int i = 0; i < r; i++) {
+                vector<int> A1;
+                for (int j = 0; j <= q; j++){
+                        val = A[index];
+                        A1.push_back(val);
+                        index++;
+                }
+                //adds vector into vector of vectors
+                newA.push_back(A1);
+        }
+
+        for (int i = r; i < k; i++) {
+                vector<int> A2;
+                for (int j = 0; j < q; j++) {
+                        val = A[index];
+                        A2.push_back(val);
+                        index++;
+                }
+                //adds vector into vector of vectors
+                newA.push_back(A2);
+        }
+
+        //need to return vector of vectors (the k subarrays)
+        return newA;
+}
+
+//-------------------------------------------------//
+// k-way merge implemented as in the assignment    //
+// if all Aj's are sorted, the output must be      //
+// sorted as well                                  //
+//-------------------------------------------------//
+vector<int> kWayMerge(const vector<vector<int>> & Aj)
+{
+        //k is the size of Aj
+        int k = Aj.size();
+        int n = 0;
+
+        //adds up all of the lengths of the vectors inside of the vector Aj
+        for (int a = 0; a < k; a++) {
+                n = n + Aj[a].size();
+        }
+
+        //creates I, which is filled with 0s at the beginning
+        vector<int> I(k);
+
+        //sets j to 1 and l to 0
+        int j = 1;
+        int l = 0;
+
+        //creates an array A
+        vector<int> A;
+
+        for (int i = 0; i < n; i++) {
+                //vector of pairs
+                vector<pair<int,int>> M;
+                for (int j = 0; j < k; j++) {
+			//checks if I[j] is not equal to the size of Aj[j]
+                        if (I[j] != Aj[j].size()) {
+                                //adds the pair to M
+                                M.push_back(make_pair(Aj[j][I[j]], j));
+                        }
+                }
+
+		//changes l and j
+                l = ArgMin(M);
+                j = M[l].second;
+
+                //adds in these values into A
+                A.push_back(Aj[j][I[j]]);
+                I[j] = I[j] + 1;
+        }
+        //return a vector, A
+        return A;
+}
+
+//-----------------------------------------//
+// mergesort                               //
+//-----------------------------------------//
+void kWayMergeSort(vector<int> & A, int k)
+{
+        //base case if array is <= 1 in length
+        int size = A.size();
+        if (size <= 1) {
+                //returns whatever is already there
+                return;
+        }
+        else {
+                //creates a vector of vectors to store result of split
+                vector<vector<int>> B = split(A, k);
+
+                //recursion
+                for (int j = 0; j < k; j++) {
+                        kWayMergeSort(B[j], k);
+                }
+                //calls kWayMerge
+                A = kWayMerge(B);
+        }
+}
+
+//-----------------------------------------//
+// reads k and the entries in A            //
+// calls kWayMergeSort and prints A        //
+// do not change anything below            //
+//-----------------------------------------//
+#ifndef TESTING
+int main()
+{
+    string line;
+    getline(cin, line);
+    std::vector<int> A;
+    int k = atoi(line.c_str());
+
+    while(true){
+        string line;
+        if(!getline(cin, line))
+            break;
+        int a = atoi(line.c_str());
+        A.push_back(a);
+    }
+
+    print(A);
+    kWayMergeSort(A, k);
+    print(A);
+}
+#endif
